c78712fb55f8293e97097b51a7dc051f6c54d480
miguel-kultur <miguel@appkultur.com>
2024-12-16 08:57:37 +0100

    Create project structure

---
+++ src/main/java/com/raffleease/raffleease/RaffleeaseApplication.java
@@ -0,0 +1,13 @@
+package com.raffleease.raffleease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaffleeaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaffleeaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raffleease/raffleease/RaffleeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raffleease.raffleease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaffleeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
