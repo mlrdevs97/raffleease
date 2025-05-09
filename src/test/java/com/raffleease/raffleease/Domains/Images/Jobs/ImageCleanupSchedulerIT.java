9fd6a32626a750af2261eaa6177c114f3c68334f
miguel-kultur <miguel@appkultur.com>
2025-05-09 18:37:14 +0200

    Fix images cleanup test

--- src/test/java/com/raffleease/raffleease/Domains/Images/Jobs/ImageCleanupSchedulerIT.java
+++ src/test/java/com/raffleease/raffleease/Domains/Images/Jobs/ImageCleanupSchedulerIT.java
@@ -3,6 +3,8 @@ package com.raffleease.raffleease.Domains.Images.Jobs;
 import com.raffleease.raffleease.Base.BaseIT;
 import com.raffleease.raffleease.Domains.Images.DTOs.ImageDTO;
 import com.raffleease.raffleease.Domains.Images.Model.Image;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -17,14 +19,21 @@ class ImageCleanupSchedulerIT extends BaseIT {
     @Autowired
     private ImageCleanupScheduler scheduler;
 
+    @Autowired
+    private EntityManager entityManager;
+
     @Test
+    @Transactional
     public void testOrphanImageCleanupDeletesOldImages() throws Exception {
         List<ImageDTO> images = parseImagesFromResponse(uploadImages(1).andReturn());
         ImageDTO image = images.get(0);
-
         Image savedImage = imagesRepository.findById(image.id()).orElseThrow();
-        savedImage.setCreatedAt(LocalDateTime.now().minusDays(1));
-        imagesRepository.save(savedImage);
+
+        entityManager.createQuery("UPDATE Image i SET i.createdAt = :createdAt WHERE i.id = :id")
+                .setParameter("createdAt", LocalDateTime.now().minusSeconds(3600))
+                .setParameter("id", savedImage.getId())
+                .executeUpdate();
+        entityManager.clear();
 
         scheduler.cleanOrphanImages();
 
