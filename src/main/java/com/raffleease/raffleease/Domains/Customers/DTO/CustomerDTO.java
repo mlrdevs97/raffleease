0ff49a740e4139c552909f9d517f005b572c06ec
miguel-kultur <miguel@appkultur.com>
2025-05-15 05:51:00 +0200

    Remove validations from CustomerDTO

--- src/main/java/com/raffleease/raffleease/Domains/Customers/DTO/CustomerDTO.java
+++ src/main/java/com/raffleease/raffleease/Domains/Customers/DTO/CustomerDTO.java
@@ -8,25 +8,13 @@ import lombok.Builder;
 
 import java.time.LocalDateTime;
 
-// TODO: Check use cases and validations
 @Builder
 public record CustomerDTO(
-        @NotBlank(message = "Must provide an id")
         String stripeId,
-
         CustomerSourceType sourceType,
-
-        @NotBlank(message = "Must provide a name for user")
         String fullName,
-
-        @NotBlank(message = "Email should not be blank")
-        @Email(message = "Invalid email")
         String email,
-
-        @NotBlank(message = "Phone number should not be blank")
-        @Pattern(regexp = "^([67])[0-9]{8}$", message = "Invalid phone number format")
         String phoneNumber,
-
         LocalDateTime createdAt,
         LocalDateTime updatedAt
 ) {
