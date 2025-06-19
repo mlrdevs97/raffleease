d058902509a55d7e785b6347aba8880bec3f6631
miguel-kultur <miguel@appkultur.com>
2025-06-19 10:00:40 +0200

    Implement soft delete endpoint for images

--- src/main/java/com/raffleease/raffleease/Domains/Images/Controller/PublicImagesController.java
+++ src/main/java/com/raffleease/raffleease/Domains/Images/Controller/PublicImagesController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/v1/associations/{associationId}/")
+@RequestMapping("/public/v1/associations/{associationId}/")
 public class PublicImagesController {
     private final ImagesService imagesService;
 
